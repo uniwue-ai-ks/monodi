a77372bfce493736e786b3948b2efa01bec6a82b
Alexander Gehrke <alexander.gehrke@uni-wuerzburg.de>
2026-05-13 14:50:15 +0200

    fix: upgrade fuseki and set default field for index to lemmas, so it does not fail if unconfigured

--- view/rdf/00_install_fuseki.sh
+++ view/rdf/00_install_fuseki.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-VERSION="6.0.0"
+VERSION="${JENA_BASE_URL:-6.1.0}"
+JENA_BASE_URL="${JENA_BASE_URL:-https://dlcdn.apache.org}"
+# use this base url
+#JENA_BASE_URL=https://archive.apache.org/dist
 
 printf "\033[1;32mInstalling Apache Jena + Fuseki $VERSION\033[0m\n"
 
-curl -fLo "apache-jena-${VERSION}.tar.gz" "https://dlcdn.apache.org/jena/binaries/apache-jena-${VERSION}.tar.gz"
-curl -fLo "apache-jena-fuseki-${VERSION}.tar.gz" "https://dlcdn.apache.org/jena/binaries/apache-jena-fuseki-${VERSION}.tar.gz"
+curl -fLo "apache-jena-${VERSION}.tar.gz" "${JENA_BASE_URL}/jena/binaries/apache-jena-${VERSION}.tar.gz"
+curl -fLo "apache-jena-fuseki-${VERSION}.tar.gz" "${JENA_BASE_URL}/jena/binaries/apache-jena-fuseki-${VERSION}.tar.gz"
 
 mkdir -p bin
 tar -x -C bin -f "apache-jena-${VERSION}.tar.gz"
